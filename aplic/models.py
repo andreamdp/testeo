c2744dcbad15d2c5f09428257bbaf8d04b4dabab
andremdp <andremdq@gmail.com>
2014-09-01 11:35:06 -0300

    agrego class Turno

--- aplic/models.py
+++ aplic/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from csvImporter.model import CsvDbModel
+class Turno(models.Model):
+    nombre=models.CharField(max_length=30)
+    def __unicode__(self):
+        return self.nombre
+
 class Persona(models.Model):
     nombre=models.CharField(max_length=30)
     def __unicode__(self):
